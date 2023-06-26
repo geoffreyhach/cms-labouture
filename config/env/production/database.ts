c9e287524e8b815af6fd925c5a2c99e107d42978
Geoffrey <95580959+geoffreyhach@users.noreply.github.com>
2023-06-26 23:17:56 +0200

    postgre test

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,19 +1,13 @@
-import { parse } from "pg-connection-string";
-const config = parse(process.env.DATABASE_URL);
-
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5931),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
     },
-    debug: false,
   },
 });
--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,7 +1,6 @@
-// Path: ./config/env/production/server.js`
 export default ({ env }) => ({
   proxy: true,
-  url: env("https://la-bouture-admin-db70be1ae035.herokuapp.com/"), // Sets the public URL of the application.
+  url: env("https://cms-labouture-production.up.railway.app/"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
   },
